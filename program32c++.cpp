e3faa1d0d9a69cee740997c9d4b06cb4210884e8
AlejandroGallegosSS <alexmontielgomez@gmail.com>
2024-06-04 20:28:25 -0600

    losdemas programas

---
+++ program32c++.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+int main (){
+	char letr;
+	cout<<"introdusca una letra";
+	cin>>letr;
+	if (letr=='a'){
+		cout<<"tu letra es una vocal ";
+	}
+		if (letr=='e'){
+		cout<<"tu letra es una vocal ";
+	}
+		if (letr=='i'){
+		cout<<"tu letra es una vocal ";
+	}
+		if (letr=='o'){
+		cout<<"tu letra es una vocal ";
+	}
+		if (letr=='u'){
+		cout<<"tu letra es una vocal ";
+	}
+}
+
